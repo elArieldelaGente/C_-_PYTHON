dbe4a635816f8423012f1e0afe38e6929f3cab67
elArieldelaGente <74423101+elArieldelaGente@users.noreply.github.com>
2025-03-02 13:17:22 -0300

    Delete C directory

--- C/library/math/celsius_to_fahr.c
+++
@@ -1,5 +0,0 @@
- /* Convert farenheit to celsius */
-
- double cel_to_fahr(double celsius){
-     return (celsius * 9.0 / 5.0) + 32.0;
- }
--- C/library/math/fahr_to_celsius.c
+++
@@ -1,5 +0,0 @@
-/* Convert farenheit to celsius */
-
-double fahr_to_cel(double fahr){
-    return (5.0 / 9.0) * (fahr - 32);
-}
